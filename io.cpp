6770673dc265aa444ae370c74cb4a342b2bc6f37
tuyanga <168737904+tuyanga@users.noreply.github.com>
2024-09-29 22:10:04 +0800

    lab3

---
+++ io.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    string name;
+
+    cout << "Enter name: ";
+    cin >> name;
+
+    cout << "Enter age: ";
+    cin >> age;
+
+    cout << "Hello, " << name << "!" << endl;
+    cout << "Your age: " << age << endl;
+
+    return 0;
+}
